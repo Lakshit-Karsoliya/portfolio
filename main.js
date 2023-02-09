e996c25c2c2a144138b6e29c7f2139c8ad55cbad
Lakshit-Karsoliya <70501687+Lakshit-Karsoliya@users.noreply.github.com>
2023-02-09 15:19:04 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("mySidebar").style.width = "90%";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-  }
